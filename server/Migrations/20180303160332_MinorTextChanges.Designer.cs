f2a3cd7e9aa569507068392b5fcf1818dc275be7
Max Cairney-Leeming <mtcairneyleeming@gmail.com>
2018-03-04 10:26:29 +0000

    Update DB, fix minor issue with lack of key

--- server/Migrations/20180303160332_MinorTextChanges.Designer.cs
+++ server/Migrations/20180303160332_MinorTextChanges.Designer.cs
@@ -11,9 +11,10 @@ using System;
 namespace server.Migrations
 {
     [DbContext(typeof(AnnotationContext))]
-    partial class AnnotationContextModelSnapshot : ModelSnapshot
+    [Migration("20180303160332_MinorTextChanges")]
+    partial class MinorTextChanges
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- server/Models/TextData.cs
+++ server/Models/TextData.cs
@@ -1,7 +1,10 @@
-﻿namespace server.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace server.Models
 {
     public class TextData
     {
+        [Key]
         public int TextId { get; set; }
         public string Title { get; set; }
         public string Abbreviation { get; set; } // abbreviation - should be same as perseus, e.g. "Cic. Mil." for the Pro Milone
