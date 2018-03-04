06d6be4e2742fabbe502d2f8bdc7160489d06453
Max Cairney-Leeming <mtcairneyleeming@gmail.com>
2018-03-04 10:26:29 +0000

    Update DB, fix minor issue with lack of key

---
+++ server/Migrations/20180303160332_MinorTextChanges.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace server.Migrations
+{
+    public partial class MinorTextChanges : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Identifier",
+                table: "Texts",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Identifier",
+                table: "Texts");
+        }
+    }
+}
