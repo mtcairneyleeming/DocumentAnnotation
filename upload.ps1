ba92cbf79f7d4364febb60a65027eeb71a1d6d73
Max Cairney-Leeming <max@cairneyleeming.co.uk>
2022-09-30 00:33:21 +0100

    new upload script

--- upload.ps1
+++ upload.ps1
@@ -1,5 +1,9 @@
-dotnet publish ./server/server.csproj -f netcoreapp2.2 -c Release
-ssh hetzner "cd /server/DocumentAnnotation; git pull; systemctl restart dotnet-docann.service "
+dotnet publish ./server/server.csproj -c Release  -p:UseAppHost=false
+ tar -c -z -f publish.tar.xz -C .\server\bin\Release\net6.0\publish -  .
+ scp -rC .\publish.tar.xz hetzner:\server\DocumentAnnotation
+
+ssh hetzner "systemctl stop dotnet-docann.service; cd /server/DocumentAnnotation; tar -xf publish.tar.xz; systemctl start dotnet-docann.service"
+
 #& 'C:\Program Files\PuTTY\plink.exe' -ssh -i 'C:\Users\Max\Local\keys\hetzner1.ppk'  root@[ip] systemctl stop kestrel-docann.service
 #& 'C:\Program Files\PuTTY\pscp.exe' -r -C  -i 'C:\Users\Max\Local\keys\hetzner1.ppk' .\server\bin\Release\netcoreapp2.2\publish\* root@[ip]:/var/aspnetcore/docann
 #& 'C:\Program Files\PuTTY\plink.exe' -ssh -i 'C:\Users\Max\Local\Keys\hetzner1.ppk'  root@[ip] systemctl start kestrel-docann.service
